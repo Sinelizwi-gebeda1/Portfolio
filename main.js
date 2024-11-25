75fab52e78f656329af73b6845d3a9493dd1dd5c
Sinelizwi-gebeda1 <sinelizwigebeda@gmail.com>
2024-11-25 09:58:43 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".looptext", {
+    strings: ["Frontend Developer", "Systems Software Developer"], // Corrected from Strings to strings
+    typeSpeed: 100,
+    backSpeed: 100, // Corrected from backspeed to backSpeed
+    backDelay: 1000,
+    loop: true,
+});
